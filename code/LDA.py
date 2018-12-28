21803c2b6bcdca4f40f6c2d354e66bcbe242fa6d
hongyi <1103009779@qq.com>
2018-12-28 15:37:20 +0800

    ,,

--- code/LDA.py
+++ code/LDA.py
@@ -5,13 +5,16 @@ from sklearn.decomposition import LatentDirichletAllocation
 def readNews():
 
     new_lists = []
-    news_path = "../data/news.txt"
+    # news_path = "../data/test2.txt"
+    news_path = "../data/process.txt"
 
     f = open(news_path,'r',encoding='utf-8')
     line = f.readline()
     N = 0
     while line:
         N = N+1
+        if N==50:
+            break;
         if line!='\n':
             new_lists.append(line)
         print("===="+str(N))
@@ -36,10 +39,18 @@ def roundMat(matrix):
     row,col = matrix.shape
     for i in range(row):
         for j in range(col):
-            matrix[i][j] = round(matrix[i][j],2)
+            matrix[i][j] = round(matrix[i][j],5)
 
     return matrix
 
+#将训练好的模型数据保存到文件里
+def saveData(mat,file):
+
+    f = open(file,"w+",encoding="utf-8")
+    for m in mat:
+        f.write(str(m)+"\n")
+
+    f.close()
 if __name__ == "__main__":
 
     #读取新闻到列表中
@@ -56,7 +67,7 @@ if __name__ == "__main__":
     print(new_tf)
 
     print("LDA模型开始------")
-    LDA = LatentDirichletAllocation(n_components=10,
+    LDA = LatentDirichletAllocation(n_components=20,
                                     learning_offset=50.,
                                     random_state=0)
     docres = LDA.fit_transform(new_tf)
@@ -64,10 +75,12 @@ if __name__ == "__main__":
     #对小数点截取
     ress = roundMat(docres)
     print(ress)
+    saveData(ress,"../data/sci_theta.txt")
     print("-----------------------------")
     resss = LDA.components_
     resss = roundMat(resss)
     print(resss)
+    saveData(resss, "../data/sci_phi.txt")
     #print(LDA.components_)
     # print(readStop())
 
